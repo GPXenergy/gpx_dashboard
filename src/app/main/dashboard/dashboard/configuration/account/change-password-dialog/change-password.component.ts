b86c167840e076f07f54015dd59d094124f2affb
Jelle Bouwhuis <jellebouwhuis@outlook.com>
2021-03-17 17:22:00 +0100

    update models, personal dashboard, change password component

--- src/app/main/dashboard/dashboard/configuration/account/change-password-dialog/change-password.component.ts
+++ src/app/main/dashboard/dashboard/configuration/account/change-password-dialog/change-password.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Validators } from '@angular/forms';
-import { Subject } from 'rxjs';
 import { SnackBarService } from '@gpx/services/snack-bar.service';
 import { AuthService } from '@gpx/services/auth.service';
 import { ModelFormBuilder, ModelFormGroup } from '@gpx/forms/model-form';
@@ -8,6 +7,7 @@ import { AuthUser } from '@gpx/models/auth-user.model';
 import { User } from '@gpx/models/user.model';
 import { UserService } from '@gpx/services/api/user.service';
 import { CustomValidators } from '@gpx/forms/custom.validator';
+import { MatDialogRef } from '@angular/material/dialog';
 
 
 @Component({
@@ -15,10 +15,8 @@ import { CustomValidators } from '@gpx/forms/custom.validator';
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.scss']
 })
-export class ChangePasswordComponent implements OnInit, OnDestroy {
+export class ChangePasswordComponent implements OnInit {
 
-
-  private readonly onDestroy: Subject<void> = new Subject();
 
   changePasswordForm: ModelFormGroup<AuthUser>;
   user: User;
@@ -30,7 +28,8 @@ export class ChangePasswordComponent implements OnInit, OnDestroy {
   show_password: boolean;
   labelType: 'above';
 
-  constructor(private modelFormBuilder: ModelFormBuilder,
+  constructor(public dialogRef: MatDialogRef<ChangePasswordComponent>,
+              private modelFormBuilder: ModelFormBuilder,
               private authService: AuthService,
               private _snackBar: SnackBarService,
               private userService: UserService) {
@@ -43,30 +42,22 @@ export class ChangePasswordComponent implements OnInit, OnDestroy {
     this.initForm();
   }
 
-  initForm() {
+  initForm(): void {
     this.changePasswordForm = this.modelFormBuilder.modelGroup(AuthUser, null, {
       password: ['', Validators.required],
       new_password: ['', Validators.required],
       confirm_password: ['', [Validators.required, CustomValidators.equalsField('new_password')]]
     });
-
-    // this.changePasswordForm.valueChanges.pipe(takeUntil(this.onDestroy)).subscribe(() => {
-    // });
-
   }
 
 
-  onSubmit() {
+  onSubmit(): void {
     // Submit if valid
     if (this.changePasswordForm.valid) {
       const passwordModel = this.changePasswordForm.getModel();
-      this.userService.updateUserPassword(passwordModel, this.user.pk).subscribe(
+      this.userService.updateUser(this.user.pk, passwordModel).subscribe(
         success => {
-          this.openSnackBar('Wachtwoord is aangepast!');
-          this.changePasswordForm = null;
-          this.formIsValid = false;
-          setTimeout(() => this.initForm(), 200);
-
+          this.dialogRef.close(true);
         },
         e => {
           if (e.error && e.status === 400) {
@@ -78,17 +69,10 @@ export class ChangePasswordComponent implements OnInit, OnDestroy {
     }
   }
 
-
-  openSnackBar(message: string) {
-    this._snackBar.success({
-      title: message,
-    });
+  onNoClick(): void {
+    this.dialogRef.close(false);
   }
 
-  ngOnDestroy(): void {
-    this.onDestroy.next();
-    this.onDestroy.complete();
-  }
 
 }
 
